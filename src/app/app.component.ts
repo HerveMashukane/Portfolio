02d3e9df20255581d4447dd25f31526562354ebc
Herve <hervemashukane@gmail.com>
2025-03-21 18:01:31 +0100

    delete no longer needer components and creat a main one

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from './navbar/navbar.component'
-import { HomeComponent } from "./home/home.component";
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, HomeComponent],
+  imports: [RouterOutlet, PortfolioComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
