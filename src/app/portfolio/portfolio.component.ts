8c7477827e1ec0b1eaafe9c863c353995e39fd97
Herve <hervemashukane@gmail.com>
2025-03-22 15:25:06 +0100

    import and render dynamically sample projects

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-portfolio',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.css'
 })
-export class PortfolioComponent {
 
+export class PortfolioComponent {
+  projects: any = [
+    {
+      text: 'hello project one',
+      image: '/assets/images/studentDashboard.png',
+    },
+    {
+      text: 'hello project one',
+      image: '/assets/images/foodieApp.png',
+    },
+    {
+      text: 'hello project one',
+      image: '/assets/images/website-template.png',
+    },
+    {
+      text: 'hello project one',
+      image: '/assets/images/todoApp.png',
+    },
+    {
+      text: 'hello project one',
+      image: '/assets/images/house-design.png',
+    },
+  ]
 }
