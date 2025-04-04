151d1cdc362d08d6e2fd85fd5dc6a15bb62755be
Herve <hervemashukane@gmail.com>
2025-04-04 17:28:46 +0100

    animate logo at 360 degree after each 3sec

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,15 @@ module.exports = {
         textPrimary: "#1E293B", // Slate Gray
         accent: "#EF4444", // Red
       },
+      keyframes: {
+        pulseLogo: {
+          '0%, 90%, 100%': { transform: 'rotate(0deg)' },
+          '90%': { transform: 'rotate(360deg)' }
+        }
+      },
+      animation: {
+        pulseLogo: 'pulseLogo 3s ease-in-out infinite'
+      }
     },
   },
   plugins: [],
