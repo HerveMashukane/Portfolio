e8414e3c2df7480e418854a011a1a57882b55f40
Herve <hervemashukane@gmail.com>
2025-04-06 09:56:21 +0100

    animate CTA btns and SM icons

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,18 +14,37 @@ module.exports = {
         accent: "#EF4444", // Red
       },
       keyframes: {
+        // logo animation
         pulseLogo: {
           '0%, 90%, 100%': {transform: 'rotate(0deg)'},
           '50%': {transform: 'rotate(360deg)'}
         },
+        // nav links animation
         bounceLinks: {
           '0%, 100%': {transform: 'translateX(0)'},
           '90%': {transform: 'translateX(-10px)'}
         },
+        // social media icons animations
+        pulseLinkedIn: {
+          '0%, 100%': {transform: 'translateY(0)'},
+          '90%': {transform: 'translateY(-10px)'}
+        },
+        pulseEmail: {
+          '0%, 100%': {transform: 'translateY(0)'},
+          '90%': {transform: 'translateY(-10px)'}
+        },
+        pulseGitHub: {
+          '0%, 100%': {transform: 'translateY(0)'},
+          '90%': {transform: 'translateY(-10px)'}
+        }
       },
+      // animations calls
       animation: {
         pulseLogo: 'pulseLogo 5s ease-in-out infinite',
-        bounceLinks: 'bounceLinks 2s ease infinite'
+        bounceLinks: 'bounceLinks 2s ease infinite',
+        bounceLinkedIn: 'pulseLinkedIn 1s ease infinite',
+        bounceEmail: 'pulseEmail 3s ease infinite',
+        bounceGitHub: 'pulseGitHub 1s ease infinite'
       }
     },
   },
