774c49aac74651daf7ce4cd312115801cde45d86
Herve <hervemashukane@gmail.com>
2026-01-21 18:39:40 +0100

    add online indicator to navbar profile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,22 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from './layout/navbar/navbar.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { SkillsPageComponent } from './pages/skills-page/skills-page.component';
 import { ProjectsPageComponent } from './pages/projects-page/projects-page.component';
 import { ContactPageComponent } from './pages/contact-page/contact-page.component';
-import { FooterComponent } from './layout/footer/footer.component';
 import { AboutPageComponent } from "./pages/about-page/about-page.component";
+import { NavbarComponent } from './pages/navbar/navbar.component';
+import { FooterComponent } from './pages/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, HomePageComponent, AboutPageComponent, SkillsPageComponent, ProjectsPageComponent, ContactPageComponent, FooterComponent],
+  imports: [NavbarComponent, 
+            HomePageComponent, 
+            AboutPageComponent, 
+            SkillsPageComponent, 
+            ProjectsPageComponent, 
+            ContactPageComponent, 
+            FooterComponent
+          ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
